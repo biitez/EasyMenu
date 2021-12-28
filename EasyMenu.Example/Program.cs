2b9faf1cabdfd219a72fa8dc31fd5c0704491382
biitez <biitecito@gmail.com>
2021-12-28 14:51:23 -0300

    added a better example usage

--- EasyMenu.Example/Program.cs
+++ EasyMenu.Example/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
 
 using EasyMenu.Models;
 
@@ -9,29 +8,40 @@ namespace EasyMenu.Example
     {
         static void Main()
         {
-            MenuConsole Menu = new MenuBuilder(BreadCrumbHeader: true, UserInputMessage: "Choose:", PageNavigationSeparator: ">")
-                .WithMenu("MenuTitle", () => { Console.WriteLine(""); } )
-                .WithMenu("MenuTitle2", async () => { await Task.Delay(1); })
-                .WithMenu("MenuTitle3", new[]
+            MenuConsole Menu = new MenuBuilder(BreadCrumbHeader: true, UserInputMessage: "Choose:", PageNavigationSeparator: "-")
+
+                // You can add lambda expressions
+                .WithMenu("Menu A", () => { Console.WriteLine("Hi from Menu A!"); } )
+
+                // or.. directly call an (a)synchronous method
+                .WithMenu("Menu MyMethod", MyMethod )
+
+                // o.. make subMenus
+                .WithMenu("Menu with SubMenus", new[]
                 {
-                    new Menu("adsasd", () => { Console.WriteLine("test"); }),
-                    new Menu("Ll", new[]
+                    // Dentro de los SubMenus puede hacer exactamente lo mismo que en .WithMenu
+                    new Menu("SubMenu A", () => { Console.WriteLine("Hi from SubMenu A!"); }),
+
+                    // Also you can create all the SubMenus you want within others
+                    new Menu("SubMenu B", new[]
                     {
-                        new Menu("other", () => { Console.WriteLine("adssda"); }),
-                        new Menu("other", () => { Console.WriteLine("adssda"); }),
-                        new Menu("other", () => { Console.WriteLine("adssda"); }),
-                        new Menu("other", () => { Console.WriteLine("adssda"); }),
-                        new Menu("other", () => { Console.WriteLine("adssda"); }),
+                        new Menu("SubSubMenu BA", () => { Console.WriteLine("Hi from SubSubMenu BA!"); }),
+                        new Menu("SubSubMenu BB", () => { Console.WriteLine("Hi from SubSubMenu BB!"); }),
                     })
                 }, false)
+                // If the user does not enter a numerical option by calling one of the Menus, this error message will appear
                 .WithCustomErrorMessageInvalidInput("Invalid Input!")
                 .Build();
 
-            
-
-            Menu.Show(false);
+            // Display menu - UpdateConsole: Refresh the console after there is an error
+            Menu.Show(UpdateConsole: true);
 
             Console.ReadLine();
+        }
+
+        static void MyMethod()
+        {
+            Console.WriteLine("Hi from MyMethod!");
         }
     }
 }
